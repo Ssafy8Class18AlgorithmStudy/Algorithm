f32ff0d7f3c3a4b84c22c1ba3fb33f31e4713a36
원종식 <jongsik1995@naver.com>
2022-08-12 13:49:42 +0900

    20292_hashmap to set

--- 2nd_week/원종식/map/src/BOJ20292.java
+++ 2nd_week/원종식/map/src/BOJ20292.java
@@ -2,14 +2,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class BOJ20292 {
     public static void main(String ...asmr) throws IOException {
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
         StringBuilder ans=new StringBuilder("");
-        HashMap<String,Integer> hashR=new HashMap<>();
-        HashMap<String,Integer> hashW=new HashMap<>();
+        Set<String> hashR=new HashSet<>();
+        Set<String> hashW=new HashSet<>();
+
         while (true){
             String get=br.readLine();
             if(get.equals("EXIT")){
@@ -21,34 +24,34 @@ public class BOJ20292 {
                 String a=st.nextToken();
                 st.nextToken();
                 String b=st.nextToken();
-                if(hashW.containsKey(a)){
+                if(hashW.contains(a)){
                     ans.append("WAIT\n");
                     hashR.clear();
                     hashW.clear();
                 }
-                else if(hashW.containsKey(b)){
+                else if(hashW.contains(b)){
                     ans.append("WAIT\n");
                     hashR.clear();
                     hashW.clear();
                 }
-                else if(hashR.containsKey(b)){
+                else if(hashR.contains(b)){
                     ans.append("WAIT\n");
                     hashR.clear();
                     hashW.clear();
                 }
                 ans.append(get).append("\n");
-                hashR.put(a,1);
-                hashW.put(b,1);
+                hashR.add(a);
+                hashW.add(b);
             }
             else {
                 String a=st.nextToken();
-                if(hashW.containsKey(a)){
+                if(hashW.contains(a)){
                     ans.append("WAIT\n");
                     hashR.clear();
                     hashW.clear();
                 }
                 ans.append(get).append("\n");
-                hashR.put(a,1);
+                hashR.add(a);
             }
         }
     }
